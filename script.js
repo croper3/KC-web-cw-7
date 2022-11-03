75e6aa24b7c90f6fc86e41ad11234b79cb66e5ec
croper3 <mohamedalouda14@gmail.com>
2022-11-03 21:05:11 +0300

    cw7

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function BMI(weight , height){
+    let result = weight / (weight * height );
+    return result;
+    console.log(BMI(7,1.8));
+}
 
+function Status(BodyMass){
+
+
+
+    if(BodyMass < 18.5){
+        return  "لديك نقص في الوزن";
+    }
+    else if (BodyMass >= 18.5 || BodyMass <= 12){
+        return "وزنك صحي";
+    }
+    else if(BodyMass >= 25){
+        return "لديك زيادة في الوزن";
+    }
+}
+
+function calculate(){
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+    let bmi_value = BMI(weight , height);
+
+    let desc = Status(bmi_value);
+    
+    let box = document.getElementById('result');
+
+    box.innerText = bmi_value + " == " + desc;
+}
